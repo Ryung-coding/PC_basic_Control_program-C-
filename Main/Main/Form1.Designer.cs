43a0fb0cfc3308a1500f3773a0a41738c0f2ad53
Ryung <ryung9514@naver.com>
2023-02-27 16:43:37 +0900

    랜덤 클래스 / 스위치 이동 / 타이머 사용

--- Main/Main/Form1.Designer.cs
+++ Main/Main/Form1.Designer.cs
@@ -28,35 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.btn = new System.Windows.Forms.Button();
+            this.msg = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // button1
+            // btn
             // 
-            this.button1.Location = new System.Drawing.Point(521, 224);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn.Location = new System.Drawing.Point(538, 57);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(74, 55);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "취소";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            this.btn.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn_MouseMove);
+            // 
+            // msg
+            // 
+            this.msg.AutoSize = true;
+            this.msg.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.msg.ForeColor = System.Drawing.Color.Red;
+            this.msg.Location = new System.Drawing.Point(28, 32);
+            this.msg.Name = "msg";
+            this.msg.Size = new System.Drawing.Size(126, 80);
+            this.msg.TabIndex = 1;
+            this.msg.Text = "10";
+            this.msg.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 20F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(160, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 34);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "초 뒤에 포맷됩니다";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(683, 451);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.msg);
+            this.Controls.Add(this.btn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label msg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Main/Main/Form1.cs
+++ Main/Main/Form1.cs
@@ -12,6 +12,9 @@ namespace Main
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();
+
+        
         public Form1()
         {
             InitializeComponent();
@@ -22,20 +25,42 @@ namespace Main
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
-            string a = "hello";
-            string b = " Ryung";
-            string c = a;
-            Console.WriteLine(c);
-            Console.WriteLine(c.Length);
 
-            bool t = false; 
+        }
 
-            if (t == false)
+        private void btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int sec = Convert.ToInt32(msg.Text); //변수형태 고치기
+            sec--;
+
+            if (sec < 0)
             {
-                Console.WriteLine(c);
+                sec = 0;
+                timer1.Enabled = false; //타이머를 중지
+                MessageBox.Show("end"); //MODAL view라고 하는 것으로 이 창 이외는 다 먹통이 된다.
+                Application.Exit(); // 프로그램 종료
             }
+
+            msg.Text = sec.ToString(); //Convert말고 이러한 방식으로도 가능하다
+
+        }
+
+        private void btn_MouseMove(object sender, MouseEventArgs e)
+        {
+            btn.Left = rnd.Next(this.Width - btn.Width);//버튼의 좌측상단이 스크린의 왼쪽 벽과의 거리를 의미함 [px]
+            btn.Top = rnd.Next(this.Height - btn.Height);//버튼의 좌측상단이 스크린의 왼쪽 벽과의 거리를 의미함 [px]
         }
     }
 }

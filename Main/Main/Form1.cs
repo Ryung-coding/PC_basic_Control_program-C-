a3cebe3fa777528d768233a202640c080276752c
Ryung <ryung9514@naver.com>
2023-02-27 15:01:05 +0900

    정수형 변환 경우

--- Main/Main/Form1.cs
+++ Main/Main/Form1.cs
@@ -21,5 +21,13 @@ namespace Main
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.MaxValue; //int.이란 int라는 클래스의 함수를 실행한다는 뜻 이 코드는 최대값을 넣는 경우
+            a++; //하나의 값을 증가시킴
+            Console.WriteLine(a); // 그 경우, 가장 최소값으로 반전이 됨
+            
+        }
     }
 }

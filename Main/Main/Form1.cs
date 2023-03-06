a0e6950585fc405a38346c866c3339e5037f4c5f
Ryung <ryung9514@naver.com>
2023-03-06 15:42:10 +0900

    날짜 요일 계산하는 GUI

--- Main/Main/Form1.cs
+++ Main/Main/Form1.cs
@@ -23,25 +23,57 @@ namespace Main
         {
 
         }
-        // double a = 15/2 -> 7이 나온다(15,2둘다 정수로 정수연산을 함)
-        // double a = 15.0/2.0 -> 7.5가 나온다
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int[] a = new int[10]; //a가 스텍이 생기고 힙에 연결됨
-            a[0] = 100; //힙에 100들어감
-            int[] b = a; // b가 스텍에 생기고, a가 가르킨 주소값을 동일하게 b도 가르키게 함
-            //그러므로 b의 원소와 a의 원소는 따로 다시 안넣어도 바뀜
-            b[0]++; //b0의 처음 원소를 바꿨기에 a[0]도 같이 바뀜
-            //a는 참조변수이기에 포인터와 다르다!
+            int y = Convert.ToInt32(text_year.Text);
+            int m = Convert.ToInt32(text_month.Text);
+            int d = Convert.ToInt32(text_day.Text);
 
-            //만일 int[] b = new int[10] 이고
-            //b = a;
-            //b[0]++;
-            //라고 할때, b의 메모리를 굳이 잡았기에 비효율적이다.    
-            
-            //자동 작다 빠르다 -> 스택(Stack) [가비지컬렉터가 알아서 메모리 처리]
-            //수동 크다 느리다 -> 힙(Heap)
+            double s = y + y/4 - y/100 + y/400 + 2.6*m + 1.6 + d;
+            //int s = y + y / 4 - y / 100 + y / 400 + (int)(2.6 * m + 1.6) + d;
+            int r = (int)s % 7;
+            //int r = s % 7;
+
+            if (m == 1)
+            {
+                y--;
+                m = 13;
+            }
+            else if( m == 2)
+            {
+                y--;
+                m = 14;
+            }
+
+            if (r == 0)
+                lab_out.Text = "일";
+
+            if (r == 2)
+                lab_out.Text = "화";
+
+            if (r == 3)
+                lab_out.Text = "수";
+
+            if (r == 4)
+                lab_out.Text = "목";
+
+            if (r == 5)
+                lab_out.Text = "금";
+
+            if (r == 6)
+                lab_out.Text = "토";
+
+            if (r == 1)
+                lab_out.Text = "월";
+
+
+
+
         }
+
+        // double a = 15/2 -> 7이 나온다(15,2둘다 정수로 정수연산을 함)
+        // double a = 15.0/2.0 -> 7.5가 나온다
+
     }
 }

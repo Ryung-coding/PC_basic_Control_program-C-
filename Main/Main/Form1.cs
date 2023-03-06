dd1ee2c4915d02a2a31d2c51bfd058c117be7fc9
Ryung <ryung9514@naver.com>
2023-03-06 15:44:39 +0900

    날짜 계산 리스트로 정리하여 만든 case

--- Main/Main/Form1.cs
+++ Main/Main/Form1.cs
@@ -30,46 +30,18 @@ namespace Main
             int m = Convert.ToInt32(text_month.Text);
             int d = Convert.ToInt32(text_day.Text);
 
-            double s = y + y/4 - y/100 + y/400 + 2.6*m + 1.6 + d;
-            //int s = y + y / 4 - y / 100 + y / 400 + (int)(2.6 * m + 1.6) + d;
-            int r = (int)s % 7;
-            //int r = s % 7;
+          
+            int s = y + y / 4 - y / 100 + y / 400 + (int)(2.6 * m + 1.6) + d;
+            int r = s % 7;
 
-            if (m == 1)
+            if (m == 1 || m ==2)
             {
                 y--;
-                m = 13;
-            }
-            else if( m == 2)
-            {
-                y--;
-                m = 14;
+                m++;
             }
 
-            if (r == 0)
-                lab_out.Text = "일";
-
-            if (r == 2)
-                lab_out.Text = "화";
-
-            if (r == 3)
-                lab_out.Text = "수";
-
-            if (r == 4)
-                lab_out.Text = "목";
-
-            if (r == 5)
-                lab_out.Text = "금";
-
-            if (r == 6)
-                lab_out.Text = "토";
-
-            if (r == 1)
-                lab_out.Text = "월";
-
-
-
-
+            String[] st = new string[7] { "일", "월", "화", "수", "목", "금", "토" };
+            lab_out.Text = st[r];
         }
 
         // double a = 15/2 -> 7이 나온다(15,2둘다 정수로 정수연산을 함)
